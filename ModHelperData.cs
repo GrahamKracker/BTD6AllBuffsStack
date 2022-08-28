0fddd22079365b53a0e1a39f850dd2d519fbd6ee
GrahamKracker <86011515+GrahamKracker@users.noreply.github.com>
2022-08-28 12:55:03 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 
 public static class ModHelperData
 {
-    public const string Name = "BTD6AllBuffsStack";
+    public const string Name = "All Buffs Stack";
     public const string Description = "Makes almost all buffs stack infinitely, also makes towers that create buffs to be affected by its own buff. The number of buffs is not displayed in the icon above the tower.";
     public const string Version = "1.0.0";
     public const string RepoOwner = "GrahamKraker";
